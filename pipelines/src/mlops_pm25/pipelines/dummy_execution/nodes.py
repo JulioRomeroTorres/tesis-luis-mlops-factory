f2108765ccd192e0bc27912718f3611adb898e73
luisromerotorres25 <luisromerotorres25@gmail.com>
2026-05-11 12:21:41 -0500

    feat: update images

--- pipelines/src/mlops_pm25/pipelines/dummy_execution/nodes.py
+++ pipelines/src/mlops_pm25/pipelines/dummy_execution/nodes.py
@@ -2,9 +2,11 @@
 This is a boilerplate pipeline 'dummy_execution'
 generated using Kedro 0.19.15
 """
-from pipelines.commons.utils import get_current_datetime
+from typing import Any
+from mlops_pm25.pipelines.commons.utils import get_current_datetime
 
-def get_datetime():
+def get_datetime(features_names: Any, start_period: str):
+    print(f"features_names: {features_names} type {type(features_names)}, start_period: {start_period} type {type(start_period)}")
     current_datetime = get_current_datetime()
     print(f"Current Datetime {current_datetime}")
     return 
